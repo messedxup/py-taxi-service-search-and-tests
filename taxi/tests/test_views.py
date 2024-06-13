c3c6cf5f2c64188576d023788f774dc8a90e5162
Serhii Khodko <118362909+messedxup@users.noreply.github.com>
2024-06-14 01:26:42 +0300

    Added tests for views

--- taxi/tests/test_views.py
+++ taxi/tests/test_views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from taxi.models import Manufacturer
+from taxi.models import Manufacturer, Driver, Car
 
 
 class PublicManufacturerTest(TestCase):
@@ -25,3 +25,79 @@ class PrivateManufacturerTest(TestCase):
         res = self.client.get(reverse("taxi:manufacturer-list"))
         self.assertEqual(res.status_code, 200)
         self.assertTemplateUsed(res, "taxi/manufacturer_list.html")
+
+
+driver_url = reverse("taxi:driver-list")
+
+
+class PublicDriverTest(TestCase):
+
+    def test_login_required(self):
+        response = self.client.get(driver_url)
+        self.assertNotEqual(response.status_code, 200)
+
+
+class PrivateDriverListViewTest(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="testuser",
+            password="test123"
+        )
+        self.client.force_login(self.user)
+
+    def test_retrieve_drivers(self):
+        Driver.objects.create(
+            username="driver1",
+            password="password1",
+            license_number="ABC12345"
+        )
+        Driver.objects.create(
+            username="driver2",
+            password="password2",
+            license_number="NBH56487"
+        )
+        response = self.client.get(driver_url)
+        self.assertEqual(response.status_code, 200)
+        drivers = Driver.objects.all()
+        self.assertEqual(
+            list(response.context["driver_list"]),
+            list(drivers)
+        )
+        self.assertTemplateUsed(response, "taxi/driver_list.html")
+
+
+car_url = reverse("taxi:car-list")
+
+
+class PublicCarTest(TestCase):
+
+    def test_login_required(self):
+        res = self.client.get(car_url)
+        self.assertNotEqual(res.status_code, 200)
+
+
+class PrivateCarListViewTest(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="testuser",
+            password="test123"
+        )
+        self.client.force_login(self.user)
+
+    def test_retrieve_cars(self):
+        manufacturer = Manufacturer.objects.create(
+            name="Toyota",
+            country="Japan"
+        )
+        Car.objects.create(model="Audi", manufacturer=manufacturer)
+        Car.objects.create(model="Corolla", manufacturer=manufacturer)
+        response = self.client.get(car_url)
+        self.assertEqual(response.status_code, 200)
+        cars = Car.objects.all()
+        self.assertEqual(
+            list(response.context["car_list"]),
+            list(cars)
+        )
+        self.assertTemplateUsed(response, "taxi/car_list.html")
