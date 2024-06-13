742972edee16e1410281ed34834ff68008aa392e
Serhii Khodko <118362909+messedxup@users.noreply.github.com>
2024-06-14 01:46:45 +0300

    another flake8 fix)

--- taxi/forms.py
+++ taxi/forms.py
@@ -3,14 +3,19 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
 from taxi.models import Car, Driver
+
+
 class CarForm(forms.ModelForm):
     drivers = forms.ModelMultipleChoiceField(
         queryset=get_user_model().objects.all(),
         widget=forms.CheckboxSelectMultiple,
     )
+
     class Meta:
         model = Car
         fields = "__all__"
+
+
 class DriverCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = Driver
@@ -19,14 +24,20 @@ class DriverCreationForm(UserCreationForm):
             "first_name",
             "last_name",
         )
+
     def clean_license_number(self):  # this logic is optional, but possible
         return validate_license_number(self.cleaned_data["license_number"])
+
+
 class DriverLicenseUpdateForm(forms.ModelForm):
     class Meta:
         model = Driver
         fields = ["license_number"]
+
     def clean_license_number(self):
         return validate_license_number(self.cleaned_data["license_number"])
+
+
 def validate_license_number(
     license_number,
 ):  # regex validation is also possible here
